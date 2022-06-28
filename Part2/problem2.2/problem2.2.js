e346fe4d8af2e0f5a95effd8862169c04d74d74a
unknown <wahyuramadhan0549@gmail.com>
2022-06-28 13:12:13 +0700

    problem2.2-Part2 was cleared

--- Part2/problem2.2/problem2.2.js
+++ Part2/problem2.2/problem2.2.js
@@ -3,3 +3,15 @@ let input1 = 6;
 let input2 = 20;
 
 // Your Code Here
+console.log(`input: ${input1}\noutput:`);
+for(let i=input1; i>0; i--){
+    if(input1%i==0){
+        console.log(i);
+    }
+}
+console.log(`input: ${input2}\noutput:`);
+for(let j=input2; j>0; j--){
+    if(input2%j==0){
+        console.log(j);
+    }
+}
