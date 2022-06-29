c2d7c8ad78ad1cd1d031548148d0f04cca5b9ea5
unknown <wahyuramadhan0549@gmail.com>
2022-06-29 19:49:06 +0700

    problem1-Part2 was cleared

--- Part2/problem1/problem1.js
+++ Part2/problem1/problem1.js
@@ -2,18 +2,18 @@
 let studentScore = 80;
 
 // Process: Your Solution Code Here
-if (studentScore >=80){
-    console.log('Nilai A');
-} else if (studentScore>=65){
-    console.log('Nilai B+');
-} else if (studentScore>=50){
-    console.log('Nilai B');
-} else if (studentScore>=35){
-    console.log('Nilai C');
-} else if (studentScore>=0){
-    console.log('Nilai D');
-} else {
-    console.log ('invalid');
+function cekNilai(nilai){
+switch (true) {
+    case nilai<0: return("Invalid"); break;
+    case nilai<35: return("Nilai D"); break;
+    case nilai<50: return("Nilai C"); break;
+    case nilai<65: return("Nilai B"); break;
+    case nilai<80: return("Nilai B+"); break;
+    case nilai<100: return("Nilai A"); break;
+    case nilai<Infinity: return("Invalid"); break;
+    default: return("Invalid"); break;
 }
+}
+console.log(cekNilai(studentScore))
 // Output
 // Nilai A
