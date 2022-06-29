b540d4f9e845075a4b79c46de245d1120094fd6d
unknown <wahyuramadhan0549@gmail.com>
2022-06-29 22:52:46 +0700

    Part3/problem1/problem1.js was clear

--- Part3/problem1/problem1.js
+++ Part3/problem1/problem1.js
@@ -1,5 +1,17 @@
-function playWithAsterik(n) {
+function playWithAsterik(n){
   // Your Code Here
-}
+  let pola= '';
 
-playWithAsterik(5);
+  for(let x= 1; x<=n; x++){
+      for(let y=n - 1; y >=x; y--){
+          pola += ' ';
+      }
+
+      for(let z=1; z<=x; z++){
+          pola += '* ';
+      }
+      pola += '\n';
+  }
+  return pola;
+}
+console.log(playWithAsterik(5));
