4c5a617c482586ce15a4de238da6ed6055a74672
unknown <wahyuramadhan0549@gmail.com>
2022-06-30 07:07:19 +0700

    problem4-part3 was cleared

--- Part3/problem4/problem4.js
+++ Part3/problem4/problem4.js
@@ -1,5 +1,17 @@
 function ubahHuruf(sentence) {
   // Your Code Here
+  let crypt = '';
+  for (let i = 0; i < sentence.length; i++) {
+    let char = sentence.charCodeAt(i);
+    if (char >= 97 && char <= 122) {
+      crypt += String.fromCharCode(((char - 97 + 10) % 26) + 97);
+    } else if (char >= 65 && char <= 90) {
+      crypt += String.fromCharCode(((char - 65 + 10) % 26) + 65);
+    } else {
+      crypt += String.fromCharCode(char);
+    }
+  }
+  return crypt;
 }
 
 console.log(ubahHuruf("SEPULSA OKE"));
