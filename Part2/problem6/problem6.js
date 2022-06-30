b375b15bf7ab8d539546d0e818f96ca52a56c1d8
unknown <wahyuramadhan0549@gmail.com>
2022-07-01 00:31:20 +0700

    problem6.js was fixed

--- Part2/problem6/problem6.js
+++ Part2/problem6/problem6.js
@@ -1,14 +1,34 @@
 function fullPrima(n) {
   // Your Code Here
-  let x = 0;
-  for (let i = 2; i <= Math.floor(n / 2); i++) {
-    x++;
-    if (n % i == 2) {
-      return false;
+  if (n<10){
+    if (n==2 ||n==3 ||n==5 ||n==7) {
+      penentu= true;
+      return true
+    } else {
+      penentu= false;
+      return false
+    }
+  } else if (n>=10) {
+    let arrBil = n.toString().split("");
+    let a = arrBil[0];
+    let b = arrBil[1];
+    if((a==2||a==3||a==5||a==7) && (b==2||b==3||b==5||b==7)){
+      penentu=true;
+      return true
+    } else {
+      penentu=false;
+      return false
     }
   }
-  return true;
 }
+
+// algoritma
+/*    if (n<10) maka (if (n=2,3,5,7) hasilnya/return true lalu else-nya hasilnya/return false)
+      else if (n>=10) maka let arrBil = n.toString().split("") // ini buat split dan jadiin array.
+      let a = arrBil[0];
+      let b = arrBil[1];
+      if (a=2,3,5, atau 7 && b= 2,3,5, atau 7) maka hasilnya true. Selain itu ya false
+      return penentu*/
 
 console.log(fullPrima(2)); // true
 console.log(fullPrima(3)); // true
