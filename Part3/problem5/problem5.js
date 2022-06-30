c35d09d1b788f0f465b0c8ed9ebd759991b70992
unknown <wahyuramadhan0549@gmail.com>
2022-06-30 07:09:27 +0700

    problem5 part3 was cleared

--- Part3/problem5/problem5.js
+++ Part3/problem5/problem5.js
@@ -1,5 +1,19 @@
 function meanMedian(arrayInput) {
   // Your Code Here
+  let result = "", mean = 0, median = 0;
+
+  mean = arrayInput.reduce((previousValue, currentValue) => 
+  previousValue + currentValue) / arrayInput.length;
+
+  let middle = Math.floor(arrayInput.length / 2);
+
+  if (arrayInput.length % 2 === 0) {
+    median = (arrayInput[middle - 1] + arrayInput[middle]) / 2;
+  } else {
+    median = arrayInput[middle];
+  } 
+  result += `${mean} ${median}`;
+  return result;
 }
 
 console.log(meanMedian([1, 2, 3, 4])); // 2.5 2.5
